c5e0de142f65d3793ae9f1c95cfdbfa710398a24
Kamil Dolkowski <kamil.dolkowski@op.pl>
2024-04-03 22:28:20 +0200

    SET IDENTITY_INSERT

--- insert.sql
+++ insert.sql
@@ -2,6 +2,7 @@
 USE LibraryPW;
 GO
 
+SET IDENTITY_INSERT  Authors ON
 INSERT INTO Authors (author_id, surename, name, biography, username)
 VALUES 
 (41,'Kochanowski','Jan','polski poeta epoki renesansu, tłumacz','Jan z Czarnolasu'),
@@ -11,17 +12,18 @@ VALUES
 (45,'Mrożek','Sławomir','polski pisarz oraz rysownik',NULL),
 (46,'Sienkiewicz','Henryk','polski nowelista, powieściopisarz i publicysta',NULL),
 (47,'Mickiewicz','Adam','polski poeta, działacz polityczny, publicysta, tłumacz, filozof, nauczyciel akademicki',NULL);
+SET IDENTITY_INSERT  Authors ON
 
-
+SET IDENTITY_INSERT  Publishing_houses ON
 INSERT INTO Publishing_houses(publishing_house_id, name)
 VALUES 
 (41, 'Czytaj z nami'),
 (42, 'Twoja Księgarnia'),
 (43, 'Księgo-teka'),
 (44, 'Lekturownik');
+SET IDENTITY_INSERT  Publishing_houses ON
 
-
-
+SET IDENTITY_INSERT  Categories ON
 INSERT INTO Categories(category_id, name)
 VALUES 
 (41, 'Liryka'),
@@ -30,24 +32,24 @@ VALUES
 (44, 'Powieść'),
 (45, 'Powieść historyczna'),
 (46, 'Epopeja');
+SET IDENTITY_INSERT  Categories ON
 
-
-
+SET IDENTITY_INSERT  Books ON
 INSERT INTO Books (book_id, author_id, description, title, publishing_year, publishing_house_id, category_id, numbers_of_books)
 VALUES 
-(41, 41, 'Zbiór fraszek, pieśni i trenów znanego polskiego poety - Jana Kochanowskiego.', 'Fraszki, pieśni i treny', 2010, 41, 41, 5),
-(42, 42, 'Polska Podziemna, Warszawa w czasach II wojny światowej.', 'Kamienie na szaniec', 2015, 42, 42, 10),
-(43, 43, 'Słynne dzieło Shakespearea. Żądza władzy, morderstwa, magia, fatum.', 'Makbet', 2011, 41, 43, 15),
-(44, 41, 'Polska tradedia renesansowa odwołująca się do mitu o wojnie trojańskiej.', 'Odprawa posłów greckich', 2020, 41, 43, 7),
-(45, 45, 'Powieść polityczna Stefana Żeromskiego wydana w 1924 w Warszawie.', 'Przedwiośnie', 1998, 42, 44, 20),
-(46, 45, 'Dramat rodzinny, satyra społeczna, groteska, konflikt pokoleń.', 'Tango', 2016, 43, 43, 6),
-(47, 46, 'Czasy potopu szwedzkiego, walka Polaków ze Szwedami.', 'Potop', 2018, 41, 45, 12),
-(48, 46, 'Czasy pierwszych chrześcijan, miłość, cierpienie, przemiana bohatera.', 'Quo vadis', 2019, 43, 45, 21),
-(49, 47, 'Epopeja narodowa, patriotyzm, przemiana bohatera.', 'Pan Taduesz', 2010, 44, 46, 13),
-(50, 47, 'Cykl dramatów romantycznych Adama Mickiewicza publikowany w latach 1823–1860.', 'Dziady', 1999, 44, 43, 14);
+(41, 41, 'Zbiór fraszek, pieśni i trenów znanego polskiego poety - Jana Kochanowskiego.', 'Fraszki, pieśni i treny', 2010, 41, 41, 1),
+(42, 42, 'Polska Podziemna, Warszawa w czasach II wojny światowej.', 'Kamienie na szaniec', 2015, 42, 42, 2),
+(43, 43, 'Słynne dzieło Shakespearea. Żądza władzy, morderstwa, magia, fatum.', 'Makbet', 2011, 41, 43, 1),
+(44, 41, 'Polska tradedia renesansowa odwołująca się do mitu o wojnie trojańskiej.', 'Odprawa posłów greckich', 2020, 41, 43, 1),
+(45, 45, 'Powieść polityczna Stefana Żeromskiego wydana w 1924 w Warszawie.', 'Przedwiośnie', 1998, 42, 44, 2),
+(46, 45, 'Dramat rodzinny, satyra społeczna, groteska, konflikt pokoleń.', 'Tango', 2016, 43, 43, 1),
+(47, 46, 'Czasy potopu szwedzkiego, walka Polaków ze Szwedami.', 'Potop', 2018, 41, 45, 1),
+(48, 46, 'Czasy pierwszych chrześcijan, miłość, cierpienie, przemiana bohatera.', 'Quo vadis', 2019, 43, 45, 2),
+(49, 47, 'Epopeja narodowa, patriotyzm, przemiana bohatera.', 'Pan Taduesz', 2010, 44, 46, 3),
+(50, 47, 'Cykl dramatów romantycznych Adama Mickiewicza publikowany w latach 1823–1860.', 'Dziady', 1999, 44, 43, 1);
+SET IDENTITY_INSERT  Books ON
 
-
-
+SET IDENTITY_INSERT  Users ON
 INSERT INTO Users(user_id, name, email, phone_number, status_id, indeks)
 VALUES 
 (41, 'Adam Miodek', 'adam.miodek@pw.edu.pl', '567845346', 1, 123345),
@@ -60,6 +62,7 @@ VALUES
 (48, 'Klaudia Dworska', 'klaudia.dworska@pw.edu.pl', '799453567', 1, 333526),
 (49, 'Kamil Wiśniewski', 'kamil.wisniewski@pw.edu.pl', '675678879', 1, 387990),
 (50, 'Adam Wierzbicki', 'adam.wierzbicki@pw.edu.pl', '590707443', 1, 234112);
+SET IDENTITY_INSERT  Users ON
 
 SET IDENTITY_INSERT  Locations ON
 INSERT INTO Locations(location_id, city, street, number, post_code)
